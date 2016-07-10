d168a3052a1ed5dee0e68ef278e19d0abd0e17ed
Mcgill <erinmc@f45c89b0babb.ant.amazon.com>
2016-07-10 19:04:45 -0400

    added the descriptions, ingredients list, and possible activities

---
+++ rainyday-fun/activities.js
@@ -0,0 +1,3 @@
+module.exports = {
+"activities": "I have the recipes to help you make the following fun rainy-day projects: playdough, potato stamps, gak, kinetic sand, paper mache, super bouncy ball,  water-proof sand, crystal needles,  ooblek, potato-powered lightbulb, secret spy writing, rock candy, volcano, and edible glass"
+}
---
+++ rainyday-fun/descriptions.js
@@ -0,0 +1,26 @@
+/**
+    Copyright 2014-2015 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+
+    Licensed under the Apache License, Version 2.0 (the "License"). You may not use this file except in compliance with the License. A copy of the License is located at
+
+        http://aws.amazon.com/apache2.0/
+
+    or in the "license" file accompanying this file. This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
+*/
+
+module.exports = {
+    "playdough": "According to Wikipedia, playdough is a modeling compound used by young children for art and craft projects at home and in school. Composed of flour, water, salt, boric acid, and mineral oil, the product was first manufactured in Cincinnati, Ohio, U.S., as a wallpaper cleaner in the 1930s.",
+    "potato stamps": " You can make your own stamps from potato. The strudy tuber allows you to make shapes, numbers, alphabet letters or any other design to create interesting prints on paper or fabric.",
+    "gak": "Gak is a non-newtonian fluid. It can flow through your fingers but, when pressure is applied, it becomes a solid.",
+    "kinetic sand": "According to Wikipedia, kinetic sand is a three-dimensional building toy made of 98% sand and 2% polydimethylsiloxane (a type of silicone oil) that mimics the physical properties of wet sand. Kinetic Sand was originally designed for sculpting so have fun!",
+    "paper mache": "Paper mache is French for chewed paper, it is a composite material consisting of paper pieces or pulp, sometimes reinforced with textiles, bound with an adhesive, such as glue, starch, or wallpaper paste. In Persia, papier-mâché has been used to manufacture small painted boxes, trays, étagères and cases. Japan and China also produced laminated paper articles using papier-mâché. In Japan and India, papier-mâché was used to add decorative elements to armor and shields.",
+    "super bouncy ball": "A super bouncy ball rebounds proportionally to the amount of force used when thrown at a hard surface. Meaning, when you throw the ball hard, it will bounce really high. Be careful!",
+    "water-proof sand": "Water proof sand is sand that doesn't get wet! When you put it into water but it will stay dry when you take it out",
+    "crystal needles": "Crystal needles that you can grow by yourself in as little as three hours. They are delicate crystals that resemble needles that you can grow in your refrigerator",
+    "ooblek": "Ooblek is a non-newtonian fluid. If you hit it fast, it feels like a solid. If you slowly put your finger in it, your finger will go in easily. Ooblek exhibits different properties depending on how much pressure is applied to it.",
+    "potato-powered lightbulb": "You heard that right. You can power a lightbulb with a potato - with a few additional tools.",
+    "secret spy writing": "Secret spy writing helps you write secret messages with friends using simple house-hold ingredients.",
+    "rock candy": "Rock candy are large crystals made of sugar that you can make at home then enjoy!",
+    "volcano": "Learn how to make a model volcano to have an erupting good time!",
+    "edible glass": "Edible glass is a sugar based candy you can make yourself at home."
+};
